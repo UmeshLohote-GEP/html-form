98f9322a0acda0f277fa3b792dcbb7cf4da402c5
Kaustubh Badrike <53936855+KaustubhBadrike-GEP@users.noreply.github.com>
2021-05-24 18:30:57 +0530

    Added HTML and JS

---
+++ update-action.js
@@ -0,0 +1,7 @@
+(() => {
+    const updateForm = (event) => {
+        const form = document.getElementById('form');
+        form.action = event.target.value || form.action;
+    }
+    document.getElementById('url').addEventListener('input', updateForm);
+})();
