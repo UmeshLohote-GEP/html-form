5856984274ccdbc7d7485b543d9d408a14af39dc
UmeshLohote-GEP <53936886+UmeshLohote-GEP@users.noreply.github.com>
2021-06-03 12:47:52 +0530

    Update update-action.js

--- update-action.js
+++ update-action.js
@@ -12,41 +12,28 @@ function convertFormToJSON(){
        
     var json = {"punchOutDetails":{
   "BuyerCode": "441003",
-  "BuyerUrl": "https://smartdev.gep.com/Catalog/PageLoad/Indexdev?oloc=108&c=NDQyMjQ10&OE=2258&PunchoutCartReqId=0&PartnerConfigId=3779#/catalog/cart",
-  "PartnerConfigurationId": 3779,
-  "BuyerCookie": "6c997e02-7851-4fea-ab86-1f677a7c09ce",
-  "Extrinsic": [
-    {
-      "Key": "UniqueName",
-      "Value": "Exxon 3 mobil 3"
-    },
-    {
-      "Key": "UserEmail",
-      "Value": "123@gep.com"
-    },
-    {
-      "Key": "Department",
-      "Value": "00A1"
-    }
-  ],
+  "BuyerUrl": "https://smartqc.gep.com/Catalog/Pageload/Index?oloc=108&c=NDQxMDAz0&OrgStruct=ent=00a0&CC=0&Source=oci&HOOK_URL=sapevent:POST&PunchoutCartReqId=0&PartnerConfigId=4557#/catalog/cart",
+  "PartnerConfigurationId": 4557,
+  "BuyerCookie": "e68c1173-4bce-47cf-bd98-2107209f84e6",
+  "Extrinsic": [],
   "Operation": "create",
   "Item": null,
   "SelectedItem": {
     "SupplierPartId": ""
   },
   "ShipTo": {
-    "addressID": "00A1",
-    "Name": "00A1 BATON ROUGE REFINERY:MPP:0944",
-    "DeliverTo": "Exxon 3 mobil 3",
-    "AddressLine1": "3545 SCENIC HIGHWAY",
+    "addressID": "00A0",
+    "Name": "00A0 BEAUMONT REFINERY:MPP:3862",
+    "DeliverTo": "danish pandhare",
+    "AddressLine1": "1795 Burt Street",
     "AddressLine2": null,
     "AddressLine3": null,
-    "City": "Baton Rouge",
-    "State": "LA",
-    "PostalCode": "70805",
+    "City": "Beaumont",
+    "State": "TX",
+    "PostalCode": "77701",
     "Country": "USA",
     "CountryCode": "US",
-    "CompanyCode": "0944"
+    "CompanyCode": "3862"
   }
 }
 };
