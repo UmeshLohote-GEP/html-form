b12833688b8144ba9a31edc3128529680d71e44f
UmeshLohote-GEP <53936886+UmeshLohote-GEP@users.noreply.github.com>
2021-06-15 22:59:44 +0530

    Update update-action.js

--- update-action.js
+++ update-action.js
@@ -10,11 +10,11 @@
 function convertFormToJSON(){
        
     var json = {"punchOutDetails":{
-  "BuyerCode": "441003",
+  "BuyerCode": "70021790",
   "BuyerUrl": "https://smartqc.gep.com/Catalog/Pageload/Index?oloc=108&c=NDQxMDAz0&OrgStruct=ent=00a0&CC=0&Source=oci&HOOK_URL=sapevent:POST&PunchoutCartReqId=0&PartnerConfigId=4557#/catalog/cart",
   "PartnerConfigurationId": 4557,
   "BuyerCookie": "e68c1173-4bce-47cf-bd98-2107209f84e6",
-  "ClientPartnerCode":"test",
+  "ClientPartnerCode":"punchouttest",
   "FailureURL":"http://scmdev.gep.com/PETRONAS/#/catalog-cart",
   "ClientName ":"PETRONAS",        
   "Extrinsic": [],
