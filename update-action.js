94ba3da7c88915c2ec7f0a8659eb863faa0064e5
UmeshLohote-GEP <53936886+UmeshLohote-GEP@users.noreply.github.com>
2021-06-17 13:45:28 +0530

    Update update-action.js

--- update-action.js
+++ update-action.js
@@ -14,6 +14,14 @@
     document.getElementById('url2').addEventListener('input', updateForm);
 })();
 
+(() => {
+    const updateForm = (event) => {
+        const form = document.getElementById('form3');
+        form.action = event.target.value || form.action;
+    }
+    document.getElementById('url3').addEventListener('input', updateForm);
+})();
+
 
 function convertFormToJSON(){
        
@@ -56,7 +64,7 @@ function convertFormToJSON2(){
   "BuyerUrl": "https://smartqc.gep.com/Catalog/Pageload/Index?oloc=108&c=NDQxMDAz0&OrgStruct=ent=00a0&CC=0&Source=oci&HOOK_URL=sapevent:POST&PunchoutCartReqId=0&PartnerConfigId=4557#/catalog/cart",
   "PartnerConfigurationId": 4557,
   "BuyerCookie": "e68c1173-4bce-47cf-bd98-2107209f84e6",
-  "ClientPartnerCode":"punchouttest2",
+  "ClientPartnerCode":"punchouttest23",
   "FailureURL":"http://scmdev.gep.com/PETRONAS/#/catalog-cart",
   "ClientName ":"PETRONAS",        
   "Extrinsic": [],
@@ -82,4 +90,38 @@ function convertFormToJSON2(){
 }
 };
    document.getElementById('text-input-value2').value=JSON.stringify(json);
+}
+function convertFormToJSON3(){
+       
+    var json = {"punchOutDetails":{
+  "BuyerCode": "70021790",
+  "BuyerUrl": "https://smartqc.gep.com/Catalog/Pageload/Index?oloc=108&c=NDQxMDAz0&OrgStruct=ent=00a0&CC=0&Source=oci&HOOK_URL=sapevent:POST&PunchoutCartReqId=0&PartnerConfigId=4557#/catalog/cart",
+  "PartnerConfigurationId": 4557,
+  "BuyerCookie": "e68c1173-4bce-47cf-bd98-2107209f84e6",
+  "ClientPartnerCode":"punchouttest2",
+  "FailureURL":"http://scmdev.gep.com/PETRONAS/#/catalog-cart",
+  "ClientName ":"PETRONAS",        
+  "Extrinsic": [],
+  "Operation": "create",
+  "Item": null,
+  "SelectedItem": {
+    "SupplierPartId": ""
+  },
+  "ShipTo": {
+    "addressID": "00A0",
+    "Name": "00A0 BEAUMONT REFINERY:MPP:3862",
+    "DeliverTo": "danish pandhare",
+    "AddressLine1": "1795 Burt Street",
+    "AddressLine2": null,
+    "AddressLine3": null,
+    "City": "Beaumont",
+    "State": "TX",
+    "PostalCode": "77701",
+    "Country": "USA",
+    "CountryCode": "US",
+    "CompanyCode": "3862"
+  }
+}
+};
+   document.getElementById('text-input-value3').value=JSON.stringify(json);
 }
