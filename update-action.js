436e7c7643971153bf00ac4f8aed14b900dcb9f1
UmeshLohote-GEP <53936886+UmeshLohote-GEP@users.noreply.github.com>
2021-06-17 11:07:16 +0530

    Update update-action.js

--- update-action.js
+++ update-action.js
@@ -41,3 +41,37 @@ function convertFormToJSON(){
 };
    document.getElementById('text-input-value').value=JSON.stringify(json);
 }
+function convertFormToJSON2(){
+       
+    var json = {"punchOutDetails":{
+  "BuyerCode": "70021790",
+  "BuyerUrl": "https://smartqc.gep.com/Catalog/Pageload/Index?oloc=108&c=NDQxMDAz0&OrgStruct=ent=00a0&CC=0&Source=oci&HOOK_URL=sapevent:POST&PunchoutCartReqId=0&PartnerConfigId=4557#/catalog/cart",
+  "PartnerConfigurationId": 4557,
+  "BuyerCookie": "e68c1173-4bce-47cf-bd98-2107209f84e6",
+  "ClientPartnerCode":"punchouttest2",
+  "FailureURL":"http://scmdev.gep.com/PETRONAS/#/catalog-cart",
+  "ClientName ":"PETRONAS",        
+  "Extrinsic": [],
+  "Operation": "create",
+  "Item": null,
+  "SelectedItem": {
+    "SupplierPartId": ""
+  },
+  "ShipTo": {
+    "addressID": "00A0",
+    "Name": "00A0 BEAUMONT REFINERY:MPP:3862",
+    "DeliverTo": "danish pandhare",
+    "AddressLine1": "1795 Burt Street",
+    "AddressLine2": null,
+    "AddressLine3": null,
+    "City": "Beaumont",
+    "State": "TX",
+    "PostalCode": "77701",
+    "Country": "USA",
+    "CountryCode": "US",
+    "CompanyCode": "3862"
+  }
+}
+};
+   document.getElementById('text-input-value2').value=JSON.stringify(json);
+}
